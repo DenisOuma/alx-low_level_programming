1f59a8958519c2cb761f1a044c67f7de85b81900
DenisOuma <dennisouma38@gmail.com>
2021-07-15 20:48:34 +0300

    Changes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,52 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * _strdup - copies a string into a newly allocated space in memory
- * @str: string to copy
- * Return: NULL if size == 0, a pointer to the array, null if
- * the memory allocation fails
+ * string_nconcat- concatenates two strings with the n bytes of the second
+ * @s1: first string
+ * @s2: string to be added
+ * @n: amount of bytes of the second string to be copied
+ * Return: Null if the memory allocation fails
  */
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *news;
-	int a, b, len = 0;
+	unsigned int len1, len2, a, b;
+	char *p;
 
-	if (str == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = _strlen(s1), len2 = _strlen(s2);
+	if (n >= len2)
+		n = len2;
+	p = malloc(sizeof(char) * (len1 + n + 1));
+	if (p == NULL)
 		return (NULL);
-	for (a = 0; str[a] != '\0'; a++)
+	for (a = 0; s1[a] != '\0'; a++)
 	{
-		len++;
+		p[a] = s1[a];
 	}
-	len += 1;
-	news = (char *) malloc(sizeof(char) * len);
-	if (news == NULL)
-		return (NULL);
-	for (b = 0; b <= len; b++)
+	for (b = 0; b < n; b++)
 	{
-		news[b] = str[b];
+		p[a + b] = s2[b];
 	}
-	return (news);
+	p[a + b] = '\0';
+	return (p);
 }
+/**
+ * _strlen - determinates the lenght of a string
+ * @s: pointer to string
+ * Return: the length
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int a;
+	unsigned int len;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	len++;
+	}
+return (a);
+}
+
